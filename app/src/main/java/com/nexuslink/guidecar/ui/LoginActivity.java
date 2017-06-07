136d7d3b459e16dd577e7b52f85173efe92c0fec
RUI1806415923 <1806415923@qq.com>
2017-06-07 23:02:27 +0800

    状态栏与导航栏

--- app/src/main/java/com/nexuslink/guidecar/ui/LoginActivity.java
+++ app/src/main/java/com/nexuslink/guidecar/ui/LoginActivity.java
@@ -1,15 +1,25 @@
 package com.nexuslink.guidecar.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.nexuslink.guidecar.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @OnClick(R.id.button) void login() {
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 }
