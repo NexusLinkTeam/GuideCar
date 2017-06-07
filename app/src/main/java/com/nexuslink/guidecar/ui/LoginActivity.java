12e448bcf177038f8a2872e991901b2f841fedad
RUI1806415923 <1806415923@qq.com>
2017-06-07 21:20:29 +0800

    简陋的登录

---
+++ app/src/main/java/com/nexuslink/guidecar/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.nexuslink.guidecar.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nexuslink.guidecar.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
